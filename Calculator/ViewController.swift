f2b1ba1dbd20f8058751dbcb5437026c611f0c49
serhatoluc <serhatoluc@hotmail.com>
2022-08-10 13:59:41 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by SERAP ÖLÜÇ on 10.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
